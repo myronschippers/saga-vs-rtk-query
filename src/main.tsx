48f4462ec3a061412676e0deac7f6e01d1d5e9ec
Myron R Schippers Jr <dubtwee@gmail.com>
2023-08-07 23:36:28 -0400

    intial configuration for using the @mui Material UI library

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { store } from './store';
 import { Provider } from 'react-redux';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import { store } from './store';
 
 import App from './App.tsx';
 import './index.css';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+const darkTheme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
+  <ThemeProvider theme={darkTheme}>
+    <CssBaseline />
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </ThemeProvider>
 );
